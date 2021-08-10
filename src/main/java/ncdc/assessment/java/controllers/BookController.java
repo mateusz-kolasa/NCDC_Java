c43b7f692c486ab9dcb280313f8e9f5d32a2286e
Mateusz Kolasa <mateusz.kolasa97@gmail.com>
2021-08-10 16:42:56 +0200

    New book form

--- src/main/java/ncdc/assessment/java/controllers/BookController.java
+++ src/main/java/ncdc/assessment/java/controllers/BookController.java
@@ -3,10 +3,17 @@ package ncdc.assessment.java.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import ncdc.assessment.java.domain.Book;
 import ncdc.assessment.java.services.BookService;
@@ -20,5 +27,19 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", bookService.getAllBooks());
 		return "list_page";
+	}
+	
+	@GetMapping("/book/add")
+	public String addRecordForm(Model model) {
+        model.addAttribute("book", new Book());
+		return "new_record_page";
+	}
+	
+	@PostMapping("/book/add")
+	public String addRecord(@ModelAttribute @Valid Book book, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "new_record_page";
+		}
+		return "redirect:/";
 	}
 }
